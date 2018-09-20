2bb3fae95787e17528c21eb3d1c366b2ee722a72
lvalandro <lvalandrorocha@hotmail.com>
2018-09-20 19:47:50 -0300

    Increase gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglifycss = require('gulp-uglifycss');
- 
+const minify = require('gulp-minify');
+
 gulp.task('sass', function () {
     return gulp.src('./public/sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('css', function () {
+gulp.task('minify-css', function () {
     return gulp.src('./public/css/*.css')
-      .pipe(uglifycss({
-        "uglyComments": true
-      }))
-      .pipe(gulp.dest('./dist/css/'));
+        .pipe(uglifycss({
+            "uglyComments": true
+        }))
+        .pipe(gulp.dest('./dist/css/'));
+});
+ 
+gulp.task('minify-js', function() {
+    return gulp.src(['./public/js/*.js', './public/js/*.mjs'])
+        .pipe(minify())
+        .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('build', gulp.series(['sass','css']));
+gulp.task('include-views', function(){
+    return gulp.src(['./public/src/**/*.*'])
+        .pipe(gulp.dest('./dist/views'));
+});
+
+gulp.task('build', gulp.series(['sass']));
+gulp.task('prod', gulp.series(['sass','minify-css','minify-js','include-views']));
 
 gulp.task('watch', function(){
     gulp.watch('./public/sass/*.scss', gulp.series('sass'));
